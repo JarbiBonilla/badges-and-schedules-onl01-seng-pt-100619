a7bbb12b5dabc853e904ae59b3ba8e1385a0acc9
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-15 05:22:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,9 @@ def batch_badge_creator(array)
   array.each {|name| new_array << "Hello, my name is #{name}."} 
   return new_array
 end  
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index {|name, index| new_array << "Hello, #{name}! You'll be assigned to room #{index}!"}
+    return new_array
+end    
