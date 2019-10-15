f02d53006a624ef91cd42c2f6183c351ac86e0bc
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-15 05:26:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms(array)
   new_array = []
-  array.each_with_index {|name, index| new_array << "Hello, #{name}! You'll be assigned to room #{index}!"}
+  array.each_with_index {|name, index| new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
     return new_array
 end    
