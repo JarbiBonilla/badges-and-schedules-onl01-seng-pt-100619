566ad08c9f30c934875e225b0330126017230691
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-15 05:11:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.each {|name| "Hello, my name is #{name}."}
+  array.each {|name| new_array << "Hello, my name is #{name}."} 
+  return new_array
 end  
